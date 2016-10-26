1b938b5ba6d7e5ed1fdffb7aa15a36bcd358163d
Vladislav Kuzmin <vladiskuz@gmail.com>
2016-10-26 12:27:42 +0300

    Sync time inside conttainers with host machine

--- dashboard/boot.sh
+++ dashboard/boot.sh
@@ -5,8 +5,8 @@ source .env
 network_id=$(docker network create ${network})
 echo "Created network id: $network_id"
 
-influx_id=$(docker run -d -p 8083:8083 -p 8086:8086 -v ${influx_name}:/var/lib/influxdb --net ${network} --name ${influx_name} influxdb:0.13-alpine)
+influx_id=$(docker run -d -p 8083:8083 -p 8086:8086 -v ${influx_name}:/var/lib/influxdb -v /etc/localtime:/etc/localtime:ro --net ${network} --name ${influx_name} influxdb:0.13-alpine)
 echo "Created InfluxDB container id: $influx_id"
 
-grafana_id=$(docker run -d -p 3000:3000 -v ${grafana_name}:/var/lib/grafana --net ${network} --name ${grafana_name} grafana/grafana)
+grafana_id=$(docker run -d -p 3000:3000 -v ${grafana_name}:/var/lib/grafana -v /etc/localtime:/etc/localtime:ro --net ${network} --name ${grafana_name} grafana/grafana)
 echo "Created Grafana container id: $grafana_id"
